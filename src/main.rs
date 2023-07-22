ae32424822865758ed710dcd6801a244c6290a83
Yutan0423 <yuutan3240@gmail.com>
2023-07-22 12:05:03 +0900

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rust_kakeibo::services;
 use std::io;
 
 fn main() {
@@ -5,7 +6,8 @@ fn main() {
     println!("実行したい内容を入力してください（0: 登録, 1: 集計）");
     io::stdin().read_line(&mut service_type).unwrap();
     let service_type: u8 = service_type.trim().parse().expect("数値を入力してください");
-    // TODO: 入力値のバリデーション
+    services::validate::InputValidator::validate_service_type(service_type);
+
     if service_type == 0 {
         println!("登録サービス");
     } else if service_type == 1 {
